f5672c6b0c63e9f71ab8e713ee74b017c606609e
Hemant-Dev <phemant092@gmail.com>
2022-03-17 12:49:19 +0530

    43e

--- eTickets/Controllers/ProducersController.cs
+++ eTickets/Controllers/ProducersController.cs
@@ -1,5 +1,6 @@
 ﻿using eTickets.Data;
 using eTickets.Data.Services;
+using eTickets.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -29,6 +30,23 @@ namespace eTickets.Controllers
             var producerDetails = await _service.GetByIdAsync(id);
             if (producerDetails == null) return View("NotFound");
             return View(producerDetails);
+        }
+
+        //Get: Producers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Post: Producers/Create
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("ProfilePictureURL,FullName,Bio")]Producer producer)
+        {
+            if(!ModelState.IsValid) return View(producer);
+
+            await _service.AddAsync(producer);
+            return RedirectToAction(nameof(Index));
+
         }
 
     }
--- eTickets/Models/Producer.cs
+++ eTickets/Models/Producer.cs
@@ -11,10 +11,17 @@ namespace eTickets.Models
     {
         [Key]
         public int Id { get; set; }
+
         [Display(Name = "Profile Picture")]
+        [Required(ErrorMessage = "Profile Picture is Required!")]
         public string ProfilePictureURL { get; set; }
+
         [Display(Name = "Full Name")]
+        [Required(ErrorMessage = "FullName is Required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Full Name must be between 3 and 50 chars long")]
         public string FullName { get; set; }
+
+        [Required(ErrorMessage = "Biography is Required")]
         [Display(Name = "Biography")]
         public string Bio { get; set; }
 
