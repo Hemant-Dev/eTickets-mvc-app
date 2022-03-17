c9c3c59ea4afa5bbf1cf744834e848adcaa727b5
Hemant-Dev <phemant092@gmail.com>
2022-03-17 14:54:20 +0530

    50e

--- eTickets/Controllers/CinemasController.cs
+++ eTickets/Controllers/CinemasController.cs
@@ -65,5 +65,24 @@ namespace eTickets.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        //Get: Cinemas/Delete/1
+        public async Task<IActionResult> Delete(int id)
+        {
+            var cinemaDetails = await _service.GetByIdAsync(id);
+            if (cinemaDetails == null) return View("NotFound");
+
+            return View(cinemaDetails);
+        }
+        //Post: Cinemas/Delete/1
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var cinemaDetails = await _service.GetByIdAsync(id);
+            if (cinemaDetails == null) return View("NotFound");
+
+            await _service.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
