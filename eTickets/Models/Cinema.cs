6d0978c4c0be703fb7e25dc3cbf93258564dde9b
Hemant-Dev <phemant092@gmail.com>
2022-03-17 14:34:45 +0530

    47e

--- eTickets/Models/Cinema.cs
+++ eTickets/Models/Cinema.cs
@@ -11,11 +11,14 @@ namespace eTickets.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required(ErrorMessage = "Cinema Logo is required")]
         [Display(Name = "Cinema Logo")]
         public string Logo { get; set; }
         [Display(Name = "Cinema Name")]
+        [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
         [Display(Name = "Description")]
+        [Required(ErrorMessage = "Description is required")]
         public string Description { get; set; }
 
         //Relationships
