646dea39053ac37a98f387a9e0c1438cb9803436
Hemant-Dev <phemant092@gmail.com>
2022-03-21 19:53:21 +0530

    56e

--- eTickets/Controllers/MoviesController.cs
+++ eTickets/Controllers/MoviesController.cs
@@ -1,6 +1,7 @@
 ﻿using eTickets.Data;
 using eTickets.Data.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -33,10 +34,15 @@ namespace eTickets.Controllers
         }
 
         //Get: Movies/Create
-        public IActionResult Create()
+        public async Task<IActionResult> Create()
         {
-            ViewData["Welcome"] = "Welcome to our Store";
-            ViewBag.msg = "Hi Welcome to our store";
+            var movieDropdownsData = await _service.GetNewMovieDropdownsValues();
+
+            ViewBag.Cinemas = new SelectList(movieDropdownsData.Cinemas, "Id", "Name");
+            ViewBag.Producers = new SelectList(movieDropdownsData.Producers, "Id", "FullName");
+            ViewBag.Actors = new SelectList(movieDropdownsData.Actors, "Id", "FullName");
+
+
             return View();
         }
 
