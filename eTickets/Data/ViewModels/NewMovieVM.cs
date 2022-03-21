4bd42e1b21d2c89cb8ac20c45543eb445c42b08a
Hemant-Dev <phemant092@gmail.com>
2022-03-21 19:29:18 +0530

    55a

---
+++ eTickets/Data/ViewModels/NewMovieVM.cs
@@ -0,0 +1,49 @@
+﻿using eTickets.Data;
+using eTickets.Data.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Models
+{
+    public class NewMovieVM
+    {
+        [Display(Name = "Movie Name")]
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Display(Name = "Movie description")]
+        [Required(ErrorMessage = "Description is required")]
+        public string Description { get; set; }
+        [Display(Name = "Price in $")]
+        [Required(ErrorMessage = "Price is required")]
+        public double Price { get; set; }
+        [Display(Name = "Movie poster URL")]
+        [Required(ErrorMessage = "Movie poster URL is required")]
+        public string ImageURL { get; set; }
+        [Display(Name = "Movie Start Date")]
+        [Required(ErrorMessage = "Start Date is required")]
+        public DateTime StartDate { get; set; }
+        [Display(Name = "Movie End Date")]
+        [Required(ErrorMessage = "End Date is required")]
+        public DateTime EndDate { get; set; }
+        [Display(Name = "Select a Category")]
+        [Required(ErrorMessage = "Movie Category is required")]
+        public MovieCategory MovieCategory { get; set; }
+
+        //Relationships
+        [Display(Name = "Select actor(s)")]
+        [Required(ErrorMessage = "Movie actor(s) is required")]
+        public List<int> ActorIds { get; set; }
+        [Display(Name = "Select a cinema")]
+        [Required(ErrorMessage = "Movie cinema is required")]
+        public int CinemaId { get; set; }
+        [Display(Name = "Select a Producer")]
+        [Required(ErrorMessage = "Movie producer is required")]
+        public int ProducerId { get; set; }
+       
+    }
+}
